f33e9f32cfdf1df25c4018e78e6e905c123d3d19
ifechukwu <ifechukwu.udorji@accelerexholdings.com>
2022-06-05 14:54:00 +0100

    Initial commit

---
+++ app/src/main/java/com/hashconcepts/composerecipeapp/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.hashconcepts.composerecipeapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Black = Color(0xFF000000)
+val Red = Color(0xFFF9173D)
+val Yellow = Color(0xFFFFC72F)
+val Grey = Color(0xFF828282)
+val White = Color(0xFFFFFFFF)

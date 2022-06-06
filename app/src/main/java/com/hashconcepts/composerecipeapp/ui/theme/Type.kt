e658407d17f4130af22792272394cdffc961e748
ifechukwu <ifechukwu.udorji@accelerexholdings.com>
2022-06-06 10:44:38 +0100

    added dependencies

--- app/src/main/java/com/hashconcepts/composerecipeapp/ui/theme/Type.kt
+++ app/src/main/java/com/hashconcepts/composerecipeapp/ui/theme/Type.kt
@@ -2,10 +2,18 @@ package com.hashconcepts.composerecipeapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.hashconcepts.composerecipeapp.R
 
+val gilroy = FontFamily(
+    listOf(
+        Font(R.font.gilroy_light, FontWeight.Light),
+        Font(R.font.gilroy_extra_bold, FontWeight.ExtraBold)
+    )
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
